328bab3f404a7ff00eb2107d9e0bd924ba1c431f
Your Name <you@example.com>
2023-05-22 21:08:41 -0300

    adicionando projeto inicial

---
+++ SecretWord/secretword/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PageOne from './Components/PageOne';
+
+function App() {
+  return (
+    <div className="App">
+        <PageOne/>
+    </div>
+  );
+}
+
+export default App;
---
+++ SecretWord/secretword/src/Components/PageOne.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageOne = () => {
+  return (
+    <div>
+        <h1>Essa é a página 1</h1>
+    </div>
+  )
+}
+
+export default PageOne;
